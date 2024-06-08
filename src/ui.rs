c1e267514ae0e5a1c0ca117eb00f0018c3010b7a
Luna5akura <734514717@qq.com>
2024-06-08 21:36:45 +0800

    beta.1

--- src/ui.rs
+++ src/ui.rs
@@ -14,7 +14,23 @@ pub struct DictionaryApp {
 
 impl DictionaryApp {
     pub(crate) fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        load_global_font(&cc.egui_ctx);
+        let mut fonts = eframe::egui::FontDefinitions::default();
+        fonts.font_data.insert(
+            "msyh".to_owned(),
+            eframe::egui::FontData::from_static(include_bytes!("C:\\Windows\\Fonts\\msyh.ttc")),
+        );
+        fonts
+            .families
+            .get_mut(&eframe::egui::FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "msyh".to_owned());
+        fonts
+            .families
+            .get_mut(&eframe::egui::FontFamily::Monospace)
+            .unwrap()
+            .push("msyh".to_owned());
+        cc.egui_ctx.set_fonts(fonts);
+
         DictionaryApp::default()
     }
 }
@@ -37,124 +53,113 @@ impl Default for DictionaryApp {
 }
 
 impl App for DictionaryApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut Frame) {
+
+        egui::CentralPanel::default().frame(egui::Frame::window(&ctx.style()).fill(egui::Color32::from_rgb(240, 240, 240))).show(ctx, |ui| {
             ui.vertical_centered(|ui| {
                 ui.heading("Japanese Dictionary Search");
                 ui.separator();
 
-                ui.horizontal(|ui| {
-                    let search_response = ui.text_edit_singleline(&mut self.query);
-
-                    if ui.button("Search").clicked() || search_response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
-                        match search_db(&self.query, 0, 20) {
-                            Ok(results) => {
-                                self.search_results = results;
-                                println!("Found {} results", self.search_results.len());
-                            }
-                            Err(e) => {
-                                println!("Error occurred while searching: {:?}", e);
-                            }
-                        }
-                    }
-
-                    if ui.button("❌").clicked() {
-                        self.query.clear();
-                    }
+                egui::Frame::none().fill(egui::Color32::from_rgb(250, 250, 250)).show(ui, |ui| {
+                    self.render_search_bar(ui);
                 });
 
                 if !self.search_results.is_empty() {
                     ui.separator();
-                    ui.label(format!("Found {} results:", self.search_results.len()));
-                    ui.add_space(10.0);
-
-                    egui::ScrollArea::vertical()
-                        .auto_shrink([false, false]) // Ensure the scroll area does not auto-shrink
-                        .show(ui, |ui| {
-                            for (i, entry) in self.search_results.iter().enumerate() {
-                                ui.vertical_centered(|ui| {
-                                    ui.group(|ui| {
-                                        ui.set_width(600.0);
-                                        ui.style_mut().visuals.widgets.inactive.bg_fill = self.bg_colors[i % self.bg_colors.len()];
-
-                                        let font_size = 20.0;
-                                        ui.style_mut().override_text_style = Some(egui::TextStyle::Body);
-
-                                        ui.horizontal(|ui| {
-                                            ui.label(egui::RichText::new(&entry.word).size(40.0).strong()).on_hover_text(format!("Pronunciation: {}", entry.pronunciation));
-                                            ui.label(format!("【{}】", &entry.reading));
-                                        });
-
-                                        ui.add_space(5.0);
-
-                                        ui.horizontal(|ui| {
-                                            ui.label(egui::RichText::new(format!("{}", entry.pos)).color(egui::Color32::LIGHT_BLUE));
-
-                                            if let Some(infl) = &entry.inflection {
-                                                ui.label(egui::RichText::new(format!("({})", infl)).color(egui::Color32::LIGHT_GREEN));
-                                            }
-
-                                            if let Some(tags) = &entry.tags {
-                                                ui.horizontal_wrapped(|ui| {
-                                                    for tag in tags.split(' ') {
-                                                        ui.label(
-                                                            egui::RichText::new(tag)
-                                                                .background_color(egui::Color32::from_rgb(224, 240, 255))
-                                                                .color(egui::Color32::from_rgb(0, 123, 255))
-                                                        ).on_hover_text("Tag explanation here");
-                                                        ui.add_space(5.0);
-                                                    }
-                                                });
-                                            }
-                                        }).response.on_hover_text(format!("Frequency: {}", entry.freq));
-
-                                        ui.add_space(10.0);
-
-                                        ui.label("Translations:");
-                                        ui.vertical(|ui| {
-                                            for tran in &entry.translations {
-                                                ui.label(format!("- {}", tran));
-                                            }
-                                        });
-                                    });
-                                });
-
-                                ui.add_space(20.0);
-                            }
-                        });
-
-                    ui.separator();
-
-                    // Pagination Controls here (if needed)
-                    // ...
+                    egui::Frame::none().fill(egui::Color32::from_rgb(245, 245, 245)).show(ui, |ui| {
+                        self.render_search_results(ui);
+                    });
                 }
             });
         });
     }
 }
 
-pub fn load_global_font(ctx: &egui::Context) {
-    let mut fonts = eframe::egui::FontDefinitions::default();
+impl DictionaryApp {
+    fn render_search_bar(&mut self, ui: &mut egui::Ui) {
+        ui.vertical_centered(|ui| {
+            let search_response = ui.text_edit_singleline(&mut self.query);
 
-    // Install my own font (maybe supporting non-latin characters):
-    fonts.font_data.insert(
-        "msyh".to_owned(),
-        eframe::egui::FontData::from_static(include_bytes!("C:\\Windows\\Fonts\\msyh.ttc")), // .ttf and .otf supported
-    );
+            if ui.button("Search").clicked() || search_response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                match search_db(&self.query, 0, 20) {
+                    Ok(results) => {
+                        self.search_results = results;
+                        println!("Found {} results", self.search_results.len());
+                    }
+                    Err(e) => {
+                        println!("Error occurred while searching: {:?}", e);
+                    }
+                }
+            }
 
-    // Put my font first (highest priority):
-    fonts
-        .families
-        .get_mut(&eframe::egui::FontFamily::Proportional)
-        .unwrap()
-        .insert(0, "msyh".to_owned());
+            if ui.button("❌").clicked() {
+                self.query.clear();
+            }
+        });
+    }
 
-    // Put my font as last fallback for monospace:
-    fonts
-        .families
-        .get_mut(&eframe::egui::FontFamily::Monospace)
-        .unwrap()
-        .push("msyh".to_owned());
+    fn render_search_results(&self, ui: &mut egui::Ui) {
+        ui.label(format!("Found {} results:", self.search_results.len()));
+        ui.add_space(10.0);
 
-    ctx.set_fonts(fonts);
+        egui::ScrollArea::vertical()
+            .auto_shrink([false, false])
+            .show(ui, |ui| {
+                for (i, entry) in self.search_results.iter().enumerate() {
+                    egui::Frame::none().fill(self.bg_colors[i % self.bg_colors.len()]).show(ui, |ui| {
+                        self.render_search_result_item(ui, entry, i);
+                    });
+                    ui.add_space(20.0);
+                }
+            });
+
+        ui.separator();
+    }
+
+    fn render_search_result_item(&self, ui: &mut egui::Ui, entry: &DictionaryEntry, index: usize) {
+        ui.vertical_centered(|ui| {
+            ui.group(|ui| {
+                ui.set_width(600.0);
+
+                ui.style_mut().override_text_style = Some(egui::TextStyle::Body);
+
+                ui.horizontal(|ui| {
+                    ui.label(egui::RichText::new(&entry.word).size(40.0).strong()).on_hover_text(format!("Pronunciation: {}", entry.pronunciation));
+                    ui.label(format!("【{}】", &entry.reading));
+                });
+
+                ui.add_space(5.0);
+
+                ui.horizontal(|ui| {
+                    ui.label(egui::RichText::new(format!("{}", entry.pos)).color(egui::Color32::LIGHT_BLUE));
+
+                    if let Some(infl) = &entry.inflection {
+                        ui.label(egui::RichText::new(format!("({})", infl)).color(egui::Color32::LIGHT_GREEN));
+                    }
+
+                    if let Some(tags) = &entry.tags {
+                        ui.horizontal_wrapped(|ui| {
+                            for tag in tags.split(' ') {
+                                ui.label(
+                                    egui::RichText::new(tag)
+                                        .background_color(egui::Color32::from_rgb(224, 240, 255))
+                                        .color(egui::Color32::from_rgb(0, 123, 255))
+                                ).on_hover_text("Tag explanation here");
+                                ui.add_space(5.0);
+                            }
+                        });
+                    }
+                }).response.on_hover_text(format!("Frequency: {}", entry.freq));
+
+                ui.add_space(10.0);
+
+                ui.label("Translations:");
+                ui.vertical(|ui| {
+                    for tran in &entry.translations {
+                        ui.label(format!("- {}", tran));
+                    }
+                });
+            });
+        });
+    }
 }
